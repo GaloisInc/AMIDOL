8d5c0319c8d37c94135fa62357b5544ed6ca96aa
Alec Theriault <alec.theriault@gmail.com>
2019-01-16 17:22:33 -0800

    IR: add expression parser & JSON serialization

---
+++ ir/src/main/scala/amidol/Math.scala
@@ -0,0 +1,96 @@
+package amidol
+
+import scala.util.parsing.combinator._
+import scala.util.parsing.input._
+import scala.util._
+
+object Math {
+
+  // TODO parametrize over number space
+  // TODO debruijn index for variables 9so you know _what_ is closed over)
+  sealed trait Expr
+
+  case class Plus(lhs: Expr, rhs: Expr) extends Expr
+  case class Mult(lhs: Expr, rhs: Expr) extends Expr
+  case class Negate(e: Expr) extends Expr
+  case class Inverse(e: Expr) extends Expr
+  case class Variable(s: Symbol) extends Expr
+  case class Literal(d: Double) extends Expr
+
+
+  object Expr extends JavaTokenParsers with PackratParsers {
+
+    // Simple arithmetic grammar with a packrat parser
+
+    private lazy val atom: PackratParser[Expr] =
+      ( floatingPointNumber           ^^ { s => Literal(s.toDouble)  }
+      | raw"[a-zA-Z][a-zA-Z_0-9]".r   ^^ { v => Variable(Symbol(v))  }
+      | "(" ~> expr <~ ")"
+      | "-" ~> atom                   ^^ { e => Negate(e) }
+      )
+
+    private lazy val factor: PackratParser[Expr] =
+      ( factor ~ "*" ~ atom          ^^ { case (l ~ _ ~ r) => Mult(l,         r )  }
+      | factor ~ "/" ~ atom          ^^ { case (l ~ _ ~ r) => Mult(l, Inverse(r)) }
+      | atom
+      )
+
+    private lazy val term: PackratParser[Expr] =
+      ( term ~ "+" ~ factor          ^^ { case (l ~ _ ~ r) => Plus(l,        r ) }
+      | term ~ "-" ~ factor          ^^ { case (l ~ _ ~ r) => Plus(l, Negate(r)) }
+      | factor
+      )
+
+    private lazy val expr: PackratParser[Expr] = term
+
+    // Parse an arithmetic expression from a string
+    def apply(input: String): Try[Expr] = parseAll(expr, input) match {
+      case Success(matched, _) => scala.util.Success(matched)
+      case Failure(msg, in) => scala.util.Failure(new Exception(s"Failed at ${in.pos}: $msg\n\n${in.pos.longString}"))
+      case Error(msg, in) => scala.util.Failure(new Exception(s"Errored at ${in.pos}: $msg\n\n${in.pos.longString}"))
+      case _ => scala.util.Failure(new Exception("Parser failed in an unexpected way"))
+    }
+
+  }
+
+  implicit class ExprOps(expr: Expr) {
+
+    // Turn into a  pretty string representation.
+    //
+    // Compatible with Python's syntax
+    def prettyPrint(precedence: Int = 0): String = {
+      val p = expr.precedence
+      def wrap(s: String): String = if (precedence > expr.precedence) { "(" + s + ")" } else { s }
+      expr match {
+        case Plus(l,Negate(r))  => wrap(l.prettyPrint(p-1) + " - " + r.prettyPrint(p-1))
+        case Plus(l,r)          => wrap(l.prettyPrint(p-1) + " + " + r.prettyPrint(p-1))
+        case Mult(l,Inverse(r)) => wrap(l.prettyPrint(p-1) + " / " + r.prettyPrint(p-1))
+        case Mult(l,r)          => wrap(l.prettyPrint(p-1) + " * " + r.prettyPrint(p-1))
+        case Negate(x)          => wrap("-" + x.prettyPrint(p-1))
+        case x: Inverse         => Mult(Literal(1), x).prettyPrint(precedence)
+        case Variable(s)        => s.toString
+        case Literal(d)         => d.toString
+      }
+    }
+
+    def precedence: Int = expr match {
+      case _: Plus => 6
+      case _: Mult => 7
+      case _: Negate => 8
+      case _: Inverse => 9
+      case _: Variable |
+           _: Literal => Int.MaxValue
+    }
+
+    def eval(vals: Map[Symbol, Double]): Double = expr match {
+      case Plus(lhs, rhs) => lhs.eval(vals) + rhs.eval(vals)
+      case Mult(lhs, rhs) => lhs.eval(vals) * rhs.eval(vals)
+      case Negate(e) => -e.eval(vals)
+      case Inverse(e) => 1.0 / e.eval(vals)
+      case Variable(s) => vals(s)
+      case Literal(d) => d
+    }
+
+  }
+
+}
---
+++ ir/src/main/scala/amidol/UiTypes.scala
@@ -0,0 +1,41 @@
+import spray.json._
+
+/// The full set of shapes and lines in the UI
+case class Graph(
+  nodes: Seq[Node],
+  links: Seq[Link]
+)
+object Graph extends DefaultJsonProtocol {
+  implicit val graphFormat: JsonFormat[Graph] = jsonFormat2(Graph.apply)
+}
+
+/// A single shape in the UI
+case class Node(
+  id: Long,
+  view: String,
+  label: String,
+  location: Point
+)
+object Node extends DefaultJsonProtocol {
+  implicit val nodeFormat: JsonFormat[Node] = jsonFormat4(Node.apply)
+}
+
+/// A location in the UI
+case class Point(
+  x: Long,
+  y: Long
+)
+object Point extends DefaultJsonProtocol {
+  implicit val pointFormat: JsonFormat[Point] = jsonFormat2(Point.apply)
+}
+
+/// And edge between two nodes in the UI
+case class Link(
+  id: Long,
+  source: Long,
+  target: Long,
+  label: String
+)
+object Link extends DefaultJsonProtocol {
+  implicit val linkFormat: JsonFormat[Link] = jsonFormat4(Link.apply)
+}
