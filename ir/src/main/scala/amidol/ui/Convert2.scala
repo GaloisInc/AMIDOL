21c6bff8ef11836bc97716120be8e69778b09103
Alec Theriault <alec.theriault@gmail.com>
2019-03-29 15:53:20 -0700

    Add support for vital dynamics

--- ir/src/main/scala/amidol/ui/Convert2.scala
+++ ir/src/main/scala/amidol/ui/Convert2.scala
@@ -26,7 +26,7 @@ object convert {
       val incomingLinks = graph.links.values.map(l => (l.to, NounId(l.from))).toMap
 
       val nouns = List.newBuilder[Noun]
-      val verbs = List.newBuilder[Conserved]
+      val verbs = List.newBuilder[Verb]
       val initialConditions = Map.newBuilder[String, Double]
 
       for (Node(id, image, label, props, x, y) <- graph.nodes.values) {
@@ -35,12 +35,25 @@ object convert {
             val paramMap = params.map { case Parameter(n,v) => n -> v.toDouble }.toMap
             initialConditions += (label -> paramMap("Initial"))
             nouns += Noun(NounId(id), math.Variable(Symbol(label)), image, ui.Point(x,y))
-          case VerbProps(rate_template, params) =>
+          case VerbProps(verb_sort, params) =>
             val paramMap = params
               .map { case Parameter(n,v) => math.Variable(Symbol(n)) -> math.Expr(v).get }
               .toMap
-            val rateExpr = math.Expr(rate_template).get.applySubstitution(paramMap)
-            verbs += Conserved(VerbId(id), incomingLinks(id), outgoingLinks(id), rateExpr)
+            verbs += (verb_sort match {
+              case Conserved(rate_template) =>
+                val rateExpr = math.Expr(rate_template).get.applySubstitution(paramMap)
+                amidol.Conserved(VerbId(id), incomingLinks(id), outgoingLinks(id), rateExpr)
+              case Unconserved(rate_in_template, rate_out_template) =>
+                val rateInExpr = math.Expr(rate_in_template).get.applySubstitution(paramMap)
+                val rateOutExpr = math.Expr(rate_out_template).get.applySubstitution(paramMap)
+                amidol.Unconserved(VerbId(id), incomingLinks(id), outgoingLinks(id), rateOutExpr, rateInExpr)
+              case Source(rate_in_template) =>
+                val rateInExpr = math.Expr(rate_in_template).get.applySubstitution(paramMap)
+                amidol.Source(VerbId(id), outgoingLinks(id), rateInExpr)
+              case Sink(rate_out_template) =>
+                val rateOutExpr = math.Expr(rate_out_template).get.applySubstitution(paramMap)
+                amidol.Sink(VerbId(id), incomingLinks(id), rateOutExpr)
+            })
         }
       }
 
@@ -48,13 +61,42 @@ object convert {
       val outputVar = math.Variable('OUTPUT)
 
       val nounMap = nouns.result.map(n => n.id -> n).toMap
-      val verbMap = verbs.result.map((v: Conserved) => {
-        val substitution = Map(
-          inputVar -> nounMap(v.source).stateVariable,
-          outputVar -> nounMap(v.target).stateVariable
-        )
-        v.id -> v.copy(rate = v.rate.applySubstitution(substitution))
-      }).toMap
+      val verbMap = verbs.result.map { 
+        case v: amidol.Conserved =>
+          val substitution = Map(
+            inputVar -> nounMap(v.source).stateVariable,
+            outputVar -> nounMap(v.target).stateVariable
+          )
+          v.id -> v.copy(
+            rate = v.rate.applySubstitution(substitution)
+          )
+
+        case v: amidol.Unconserved =>
+          val substitution = Map(
+            inputVar -> nounMap(v.source).stateVariable,
+            outputVar -> nounMap(v.target).stateVariable
+          )
+          v.id -> v.copy(
+            rateOut = v.rateOut.applySubstitution(substitution),
+            rateIn = v.rateIn.applySubstitution(substitution)
+          )
+
+        case v: amidol.Source =>
+          val substitution = Map(
+            outputVar -> nounMap(v.target).stateVariable
+          )
+          v.id -> v.copy(
+            rateIn = v.rateIn.applySubstitution(substitution)
+          )
+
+        case v: amidol.Sink =>
+          val substitution = Map(
+            inputVar -> nounMap(v.source).stateVariable,
+          )
+          v.id -> v.copy(
+            rateOut = v.rateOut.applySubstitution(substitution)
+          )
+      }.toMap
 
       (Model(nounMap, verbMap), initialConditions.result)
     }
