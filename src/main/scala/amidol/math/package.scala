e0629d968dd5d5615f6b8d12369f7d3f38afa388
Alec Theriault <alec.theriault@gmail.com>
2019-07-01 11:19:42 -0400

    Add some predicates in the math language
    For now, these are all "Double" typed.
    
      false => 0.0
      true  => anything not 0.0
    
    This will eventually be fixed.

--- src/main/scala/amidol/math/package.scala
+++ src/main/scala/amidol/math/package.scala
@@ -10,6 +10,10 @@ package object math {
   // TODO debruijn index for variables 9so you know _what_ is closed over)
   sealed trait Expr
 
+  case class Not(e: Expr) extends Expr
+  case class And(lhs: Expr, rhs: Expr) extends Expr
+  case class Or(lhs: Expr, rhs: Expr) extends Expr
+  case class Comparision(lhs: Expr, op: ComparisionOp, rhs: Expr) extends Expr
   case class Plus(lhs: Expr, rhs: Expr) extends Expr
   case class Mult(lhs: Expr, rhs: Expr) extends Expr
   case class Negate(e: Expr) extends Expr
@@ -17,6 +21,10 @@ package object math {
   case class Variable(s: Symbol) extends Expr
   case class Literal(d: Double) extends Expr
 
+  sealed trait ComparisionOp
+  case object GT extends ComparisionOp
+  case object EQ extends ComparisionOp
+  case object LT extends ComparisionOp
 
   object Expr extends JavaTokenParsers with PackratParsers {
 
@@ -25,8 +33,9 @@ package object math {
       lazy val atom: PackratParser[Expr] =
         ( floatingPointNumber           ^^ { s => Literal(s.toDouble)  }
         | raw"(?U)\p{L}[\p{L}_]*".r             ^^ { v => Variable(Symbol(v))  }
-        | "(" ~> term <~ ")"
+        | "(" ~> disj <~ ")"
         | "-" ~> atom                   ^^ { e => Negate(e) }
+        | "!" ~> atom                   ^^ { e => Not(e) }
         )
 
       lazy val factor: PackratParser[Expr] =
@@ -41,7 +50,24 @@ package object math {
         | factor
         )
 
-      term
+      lazy val comp: PackratParser[Expr] =
+        ( term ~ ">"          ~ term   ^^ { case (l ~ _ ~ r) => Comparision(l, GT, r) }
+        | term ~ ("==" | "=") ~ term   ^^ { case (l ~ _ ~ r) => Comparision(l, EQ, r) }
+        | term ~ "<"          ~ term   ^^ { case (l ~ _ ~ r) => Comparision(l, LT, r) }
+        | term
+        )
+
+      lazy val conj: PackratParser[Expr] =
+        ( conj ~ ("&" | "&&") ~ comp   ^^ { case (l ~ _ ~ r) => And(l, r) }
+        | comp
+        )
+
+      lazy val disj: PackratParser[Expr] =
+        ( disj ~ ("|" | "||") ~ conj   ^^ { case (l ~ _ ~ r) => Or(l, r) }
+        | conj
+        )
+
+      disj
     }
 
     // Parse an arithmetic expression from a string
@@ -66,11 +92,17 @@ package object math {
       val p = expr.precedence
       def wrap(s: String): String = if (precedence > expr.precedence) { "(" + s + ")" } else { s }
       expr match {
+        case Or(l,r)            => wrap(l.prettyPrint(p) + " || " + r.prettyPrint(p+1))
+        case And(l,r)           => wrap(l.prettyPrint(p) + " && " + r.prettyPrint(p+1))
+        case Comparision(l,GT,r)=> wrap(l.prettyPrint(p) + " > " + r.prettyPrint(p+1))
+        case Comparision(l,EQ,r)=> wrap(l.prettyPrint(p) + " == " + r.prettyPrint(p+1))
+        case Comparision(l,LT,r)=> wrap(l.prettyPrint(p) + " M " + r.prettyPrint(p+1))
         case Plus(l,Negate(r))  => wrap(l.prettyPrint(p) + " - " + r.prettyPrint(p+1))
         case Plus(l,r)          => wrap(l.prettyPrint(p) + " + " + r.prettyPrint(p+1))
         case Mult(l,Inverse(r)) => wrap(l.prettyPrint(p) + " / " + r.prettyPrint(p+1))
         case Mult(l,r)          => wrap(l.prettyPrint(p) + " * " + r.prettyPrint(p+1))
         case Negate(x)          => wrap("-" + x.prettyPrint(p))
+        case Not(x)             => wrap("!" + x.prettyPrint(p))
         case x: Inverse         => Mult(Literal(1), x).prettyPrint(precedence)
         case Variable(s)        => s.name
         case Literal(d)         => d.toString
@@ -88,21 +120,36 @@ package object math {
     }
 
     def precedence: Int = expr match {
+      case _: Or => 3
+      case _: And => 4
+      case _: Comparision => 5
       case _: Plus => 6
       case _: Mult => 7
-      case _: Negate => 8
+      case _: Negate |
+           _: Not => 8
       case _: Inverse => 9
       case _: Variable |
            _: Literal => Int.MaxValue
     }
 
-    def eval(vals: Map[Symbol, Double] = Map()): Double = expr match {
-      case Plus(lhs, rhs) => lhs.eval(vals) + rhs.eval(vals)
-      case Mult(lhs, rhs) => lhs.eval(vals) * rhs.eval(vals)
-      case Negate(e) => -e.eval(vals)
-      case Inverse(e) => 1.0 / e.eval(vals)
-      case Variable(s) => vals.getOrElse(s, throw new Exception(s"Unbound variable $s"))
-      case Literal(d) => d
+    def eval(vals: Map[Symbol, Double] = Map()): Double = {
+      implicit def boolToDouble(b: Boolean): Double = if (b) 1.0 else 0.0
+      implicit def doubleToBool(d: Double): Boolean = d != 0.0
+
+      expr match {
+        case Not(e) => !e.eval(vals)
+        case And(lhs, rhs) => lhs.eval(vals) && rhs.eval(vals)
+        case Or(lhs, rhs) => lhs.eval(vals) || rhs.eval(vals)
+        case Comparision(lhs, LT, rhs) => lhs.eval(vals) < rhs.eval(vals)
+        case Comparision(lhs, EQ, rhs) => lhs.eval(vals) == rhs.eval(vals)
+        case Comparision(lhs, GT, rhs) => lhs.eval(vals) > rhs.eval(vals)
+        case Plus(lhs, rhs) => lhs.eval(vals) + rhs.eval(vals)
+        case Mult(lhs, rhs) => lhs.eval(vals) * rhs.eval(vals)
+        case Negate(e) => -e.eval(vals)
+        case Inverse(e) => 1.0 / e.eval(vals)
+        case Variable(s) => vals.getOrElse(s, throw new Exception(s"Unbound variable $s"))
+        case Literal(d) => d
+      }
     }
 
     def renameVariables(renamer: Renamer[Variable, Variable]): Expr = 
@@ -112,6 +159,10 @@ package object math {
       mapVariables((v: Variable) => theta.getOrElse(v, v))
 
     def mapVariables(func: Variable => Expr): Expr = expr match {
+      case Not(e) => Not(e.mapVariables(func))
+      case And(lhs, rhs) => And(lhs.mapVariables(func), rhs.mapVariables(func))
+      case Or(lhs, rhs) => Or(lhs.mapVariables(func), rhs.mapVariables(func))
+      case Comparision(lhs, op, rhs) => Comparision(lhs.mapVariables(func), op, rhs.mapVariables(func))
       case Plus(lhs, rhs) => Plus(lhs.mapVariables(func), rhs.mapVariables(func))
       case Mult(lhs, rhs) => Mult(lhs.mapVariables(func), rhs.mapVariables(func))
       case Negate(e) => Negate(e.mapVariables(func))
