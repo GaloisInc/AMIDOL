1e4910ad47c357bf3247da4c27d5e4e2da7c7468
Alec Theriault <alec.theriault@gmail.com>
2019-08-22 10:28:14 -0400

    Progress towards plotting a sum of models
    For the demo, we will want to be able to plot a _sum_ of data traces.
    The two challenges here are
    
      * how to have a good UI interface
      * how to deal with summing data traces with different domains
    
    This commits tackles a bit of both. For the we switch to using a table
    based format (where the data trace cell will eventually support multiple
    lines of summed data traces). For summing data traces, I started by
    adding functionality for the simplest thing possible: sum up series by
    taking the union of their domains, doing linear interpolation on missing
    points.

---
+++ src/main/resources/web/trace.js
@@ -0,0 +1,67 @@
+/** Returns a new summed data trace, with `x` being the union of `x` values from
+ *  the two input traces
+ */
+function addTraces(trace1, trace2) {
+  var x1 = trace1.x;
+  var x2 = trace2.x;
+  var y1 = trace1.y;
+  var y2 = trace2.y;
+
+  // Summed trace
+  var x3 = [];
+  var y3 = [];
+
+  var i1 = 0;  // index through trace1
+  var i2 = 0;  // index through trace2
+  while (i1 < x1.length || i2 < x2.length) {
+    if (x1[i1] == null) {
+      // We've run out of entries in `trace1`...
+      x3.push(x2[i2]);
+      y3.push(y2[i2]);
+      i2++;
+		} else if (x2[i2] == null) {
+      // We've run out of entries in `trace2`...
+      x3.push(x1[i1]);
+      y3.push(y1[i1]);
+      i1++;
+		} else if (x1[i1] == x2[i2]) {
+      // They share the same `x` value, so just add the `y` values
+      x3.push(x1[i1]);
+      y3.push(y1[i1] + y2[i2]);
+      i2++;
+      i1++;
+		} else if (x1[i1] < x2[i2] && i2 == 0) {
+      // We've not started values in `trace2`
+      x3.push(x1[i1]);
+      y3.push(y1[i1]);
+      i1++;
+		} else if (x2[i2] < x1[i1] && i1 == 0) {
+      // We've not started values in `trace1`
+      x3.push(x2[i2]);
+      y3.push(y2[i2]);
+      i2++;
+		} else if (x1[i1] < x2[i2]) {
+      // interpolate the value for `trace2`
+      var mult = (x1[i1] - x2[i2-1]) / (x2[i2] - x2[i2-1]);
+      var y2_interp = (y2[i2] - y2[i2-1]) * mult + y2[i2-1];
+      x3.push(x1[i1]);
+      y3.push(y1[i1] + y2_interp);
+      i1++;
+		} else if (x2[i2] < x1[i1]) {
+      // interpolate the value for `trace1`
+      var mult = (x2[i2] - x1[i1-1]) / (x1[i1] - x1[i1-1]);
+      var y1_interp = (y1[i1] - y1[i1-1]) * mult + y1[i1-1];
+      x3.push(x2[i2]);
+      y3.push(y2[i2] + y1_interp);
+      i2++;
+		}
+	}
+
+  return {
+    name: trace1.name + " + " + trace2.name,
+    mode: trace1.mode || trace2.mode,
+    x: x3,
+    y: y3,
+	}
+}
+
