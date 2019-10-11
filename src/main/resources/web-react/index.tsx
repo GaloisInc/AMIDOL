f054f17daef20cd866551ad6906b64e48b348fd9
Alec Theriault <alec.theriault@gmail.com>
2019-10-11 13:11:35 -0400

    Re-write palette editor in react
    Also hook the reward variables into the existing UI.

--- src/main/resources/web-react/index.tsx
+++ src/main/resources/web-react/index.tsx
@@ -1,23 +1,47 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello, Greet, Measures, MeasuresCallbacks } from "./components/Hello";
-
-
-/** Mount the "Dialog" onto the specified DOM node */
-export function showModalDialog(
-  mountPoint: HTMLElement,
-) {
-  const techStack = ['React', 'Typescript', 'Webpack', 'Bootstrap'];
-  ReactDOM.render(<Greet techs={techStack} />, mountPoint);
- // ReactDOM.render(<Hello />, mountPoint);
-}
+import { Hello, Greet } from "./components/Hello";
+import { Measures, MeasuresCallbacks, MeasureProps } from "./components/RewardVariables";
+import { PaletteEditor, PaletteItem } from "./components/PaletteEditor";
 
 declare var reactCallbacks: MeasuresCallbacks;
 
-
 export function showMeasures(
   mountPoint: HTMLElement,
+  setMeasure: (name: string, oldP: MeasureProps, newP: MeasureProps) => void,
 ) {
-    ReactDOM.render(<Measures callbacks={reactCallbacks}/>, mountPoint);
+  ReactDOM.render(
+    <Measures callbacks={reactCallbacks} setMeasure={setMeasure}/>,
+    mountPoint
+  );
 }
+
+/** Mount the "Palette element editor" onto the specified DOM node */
+export function showEditNodeDialog(
+  mountPoint: HTMLElement,
+
+  submitPaletteElement: (string, item, then) => void,
+  deletePaletteElement: (string, then) => void,
+
+  paletteItems: { [itemName: string]: PaletteItem },
+  chosenPaletteItemNames: string[],
+  editOnOpenItemName: string // undefined if none initially loaded
+) {
+  const close = () => ReactDOM.unmountComponentAtNode(mountPoint);
+
+  ReactDOM.render(
+    <PaletteEditor
+      closeEditor={close}
+      submitPaletteElement={submitPaletteElement}
+      deletePaletteElement={deletePaletteElement}
+
+      paletteItems={paletteItems}
+      chosenPaletteItemNames={chosenPaletteItemNames}
+      editOnOpenItemName={editOnOpenItemName}
+    />,
+    mountPoint,
+  );
+}
+
+
