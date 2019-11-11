1e9f2253998a122a7e9b05aeb9a6d8bdd43ad595
Alec Theriault <alec.theriault@gmail.com>
2019-11-11 18:21:44 -0500

    More advanced model comparision editting
    It is now possible to write arbitrary operations on traces in the
    comparision UI.

--- src/main/resources/web-react/components/Compare.tsx
+++ src/main/resources/web-react/components/Compare.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 import {
   Button, Input, Col, Form, FormGroup, Label, Row,
-  ListGroup, ListGroupItem, ListGroupItemHeading, Table
+  ListGroup, ListGroupItem, ListGroupItemHeading, Table,
+  TabPane, TabContent
 } from 'reactstrap';
 import { addTraces } from '../utility/Traces.ts';
 import { GraphResults } from './GraphResults';
 
 export interface TraceSum {
   key: string;
-  traces: string[];
+
+  // One of `traces` and `equation` is expected to be non-null
+  traces?: string[];
+  equation?: string;
 }
 
 interface CompareState {
@@ -44,7 +48,10 @@ export class Compare extends React.Component<{}, CompareState> {
   produceChartData(): Promise<any[]> {
     return Promise.all(this.state.plannedPlot.map(dataTrace => {
       const data = new URLSearchParams();
-      data.append("query", dataTrace.traces.map(x => '`' + x + '`').join("+"));
+      const equation = (dataTrace.equation)
+        ? dataTrace.equation
+        : dataTrace.traces.map(x => '`' + x + '`').join("+")
+      data.append("query", equation);
       return fetch("/appstate/data-traces/eval", { method: 'POST', body: data })
         .then(resp => resp.json())
         .then(resp => {
@@ -143,6 +150,32 @@ interface DataRowProps {
 
 export class DataRow extends React.Component<DataRowProps, {}> {
 
+  parseEquation(eqn: string): string[] | null {
+    let remainingEqn = eqn.trim();
+
+    // The empty case is special
+    if (remainingEqn === "") {
+      return [];
+    }
+
+    const variableRegex = /^\+\s*`([^`]+)`\s*(.*)$/;
+    const wip = [];
+    remainingEqn = "+" + remainingEqn;
+
+    // As long as there is input, parse a `+ <variable>` off the front.
+    while (remainingEqn != "") {
+      const match = remainingEqn.match(variableRegex);
+
+      // Failed parse
+      if (!match) return null;
+
+      wip.push(match[1]);
+      remainingEqn = match[2];
+    }
+
+    return wip;
+  }
+
   render() {
     const addAddend = () => {
       const dataTraces = this.props.getDataTraces();
@@ -167,41 +200,93 @@ export class DataRow extends React.Component<DataRowProps, {}> {
       this.props.updateTraceSum({ ...this.props.plannedTrace, traces: newTraces });
     };
 
+    const updateEquation = (newEquation: string) => {
+      this.props.updateTraceSum({ ...this.props.plannedTrace, equation: newEquation });
+    };
+
+    let disableSwapButton = false;
+    let swapButtonText = "";
+    let onSwapClick = () => { };
+
+    if (this.props.plannedTrace.traces) {
+      swapButtonText = "Advanced";
+
+      onSwapClick = () => this.props.updateTraceSum({
+        key: this.props.plannedTrace.key,
+        traces: null,
+        equation: this.props.plannedTrace.traces.map(x => "`" + x + "`").join(" + "),
+      });
+    } else {
+      swapButtonText = "Simple";
+
+      const traces = this.parseEquation(this.props.plannedTrace.equation);
+      if (traces !== null) {
+        onSwapClick = () => this.props.updateTraceSum({
+          key: this.props.plannedTrace.key,
+          traces,
+          equation: null,
+        });
+      } else {
+        disableSwapButton = true;
+      }
+    }
+
+   const editorCell = (swapButtonText === "Advanced")
+     ? (
+       <td>
+       {
+         this.props.plannedTrace.traces.map((trace: string, idx: number) => (<Row>
+           <Col><Input
+             type="select"
+             value={trace}
+             onChange={(e) => updateAddend(idx, e.target.value)}
+           >
+             {
+               this.props.getDataTraces().map(dataTraceName => <option>{dataTraceName}</option>)
+             }
+           </Input></Col>
+
+           <Col><Button
+             color="secondary"
+             onClick={() => removeAddend(idx)}
+           >Remove trace</Button>
+           &nbsp;&nbsp;{'+'}</Col>
+           </Row>
+         ))
+       }
+       <Row><Col><Button
+         color="primary"
+         onClick={addAddend}
+       >...</Button></Col></Row>
+       </td>
+     )
+     : (
+       <td>
+         <Input
+           onChange={(e) => updateEquation(e.target.value)}
+           value={this.props.plannedTrace.equation}
+         />
+       </td>
+     );
+
+
     return (
       <tr>
         <td>
-          <Input sm={6} 
+          <Input sm={6}
             value={this.props.plannedTrace.key}
             onChange={(e) => this.props.updateTraceSum({ ...this.props.plannedTrace, key: e.target.value })}
           />
         </td>
+        {editorCell}
         <td>
-          {
-            this.props.plannedTrace.traces.map((trace: string, idx: number) => (<Row>
-              <Col><Input
-                type="select"
-                onChange={(e) => updateAddend(idx, e.target.value)}
-              >
-                {
-                  this.props.getDataTraces().map(dataTraceName => <option>{dataTraceName}</option>)
-                }
-              </Input></Col>
-        
-              <Col><Button
-                color="secondary"
-                onClick={() => removeAddend(idx)}
-              >Remove trace</Button>
-              &nbsp;&nbsp;{'+'}</Col>
-              </Row>
-            ))
-          }
-          <Row><Col><Button
-            color="primary"
-            onClick={addAddend}
-          >...</Button></Col></Row>
-        </td>
-        <td>
-          <Button 
+          <Button
+            color="secondary"
+            onClick={onSwapClick}
+            disabled={disableSwapButton}
+          >{swapButtonText}</Button>
+          &nbsp;&nbsp;
+          <Button
             color="secondary"
             onClick={this.props.deleteTraceSum}
           >Remove series</Button>
